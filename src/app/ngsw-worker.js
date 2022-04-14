9741cd6cc1324c86eb77e0c07be63c4f934f9628
Shreya Shetye <shreyashetye2017@gmail.com>
2022-04-14 14:25:46 +0530

    Auto-generated commit [ci skip]

--- src/app/ngsw-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', event => {
-    console.log('Service worker installing...');
-    // Add a call to skipWaiting here
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-    console.log('Service worker activating...');
-});
